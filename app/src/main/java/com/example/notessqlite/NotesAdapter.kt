2702c3a142ea0b319f8241da7e71ea3b008fe850
Purna chandra <purna90857@gmail.com>
2024-06-26 23:20:20 +0530

    first change

---
+++ app/src/main/java/com/example/notessqlite/NotesAdapter.kt
@@ -0,0 +1,59 @@
+package com.example.notessqlite
+
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+
+class NotesAdapter(private var notes: List<Note>, context: Context) :
+    RecyclerView.Adapter<NotesAdapter.NoteViewHolder>() {
+
+        private val db:NoteDatabaseHelper=NoteDatabaseHelper(context)
+
+    class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val titleTextView: TextView = itemView.findViewById(R.id.titleTextView)
+        val contextTextView: TextView = itemView.findViewById(R.id.contentTextView)
+        val notesCardView: CardView = itemView.findViewById(R.id.notesCardView)
+        val deleteButton:ImageView=itemView.findViewById(R.id.deleteButton)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.note_item, parent, false)
+        return NoteViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return notes.size
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        val note = notes[position]
+        holder.titleTextView.text = note.title
+        holder.contextTextView.text = note.content
+
+        holder.notesCardView.setOnClickListener {
+            val intent=Intent(holder.itemView.context,UpdateNoteActivity::class.java).apply {
+                putExtra("note_id",note.id)
+            }
+            holder.itemView.context.startActivity(intent)
+        }
+
+        holder.deleteButton.setOnClickListener {
+            db.deleteNote(note.id)
+            refreshData(db.getAllNotes())
+            Toast.makeText(holder.itemView.context,"Successfully deleted",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun refreshData(newNotes: List<Note>){
+        notes=newNotes
+        notifyDataSetChanged()
+    }
+
+}
